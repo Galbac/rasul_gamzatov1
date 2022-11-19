aad017d10c53374b6f0c57168cdae7c64140c750
Galbac <97892289+Galbac@users.noreply.github.com>
2022-11-20 00:52:45 +0300

    .

--- lib/screen/bio.dart
+++ lib/screen/bio.dart
@@ -17,7 +17,7 @@ class _BioState extends State<Bio> {
       ));
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context)  {
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: AppBar(
